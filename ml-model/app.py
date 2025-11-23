4e51513e33002c4a892f7919c010ec2c4de8dabb
smv1256 <117322465+smv1256@users.noreply.github.com>
2025-11-23 11:20:09 -0500

    api endpoint

--- ml-model/app.py
+++ ml-model/app.py
@@ -8,25 +8,27 @@ app = FastAPI()
 with open("model/rf_model.pkl", "rb") as m:
     rf = joblib.load(m)
 
-# features = ["koi_period", "koi_impact", "koi_duration", "koi_depth", "koi_prad", "koi_model_snr", "koi_steff", "koi_slogg", "koi_srad", "koi_fpflag_nt", "koi_fpflag_co", "koi_fpflag_ss", "koi_fpflag_ec"]
+class PredictionOutput(BaseModel):
+    prediction: str
 
 class InputData(BaseModel):
-    koi_period: float = None
-    koi_impact: float = None
-    koi_duration: float = None
-    koi_depth: float = None
-    koi_prad: float = None
-    koi_model_snr: float = None
-    koi_steff: float = None
-    koi_slogg: float = None
-    koi_srad: float = None
-    koi_fpflag_nt: int = None
-    koi_fpflag_co: int = None
-    koi_fpflag_ss: int = None
-    koi_fpflag_ec: int = None
+    koi_period: float | None = None
+    koi_impact: float | None = None
+    koi_duration: float | None = None
+    koi_depth: float | None = None
+    koi_prad: float | None = None
+    koi_model_snr: float | None = None
+    koi_steff: float | None = None
+    koi_slogg: float | None = None
+    koi_srad: float | None = None
+    koi_fpflag_nt: int | None = None
+    koi_fpflag_co: int | None = None
+    koi_fpflag_ss: int | None = None
+    koi_fpflag_ec: int | None = None
 
 @app.post("/predict")
 def predict(data: InputData):
     d = pd.DataFrame([data.model_dump()])
     d.fillna(d.median(), inplace = True)
-    return {"prediction": rf.predict(d)[0]}
+    pred = "Exoplanet" if (rf.predict(d)[0] == 1) else "Not exoplanet"
+    return { "prediction": pred }
