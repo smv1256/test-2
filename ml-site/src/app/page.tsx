cdd4643c58b836d4b464060eff7e02ca55f4238e
smv1256 <117322465+smv1256@users.noreply.github.com>
2025-11-24 22:12:00 -0500

    move to src

--- ml-site/src/app/page.tsx
+++ ml-site/src/app/page.tsx
@@ -1,7 +1,10 @@
+import Form from "../components/form";
+
 export default function Home() {
   return (
       <main className="">
         <h1>Hi there hello</h1>
+        <Form />
       </main>
   );
 }
